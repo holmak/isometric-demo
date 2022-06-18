acdd18dd6f0e112c86b40f380f52b456539703dc
Keith Holman <holmak@gmail.com>
2022-06-18 16:02:35 -0700

    Click to edit the map.

--- Game/Game.cs
+++ Game/Game.cs
@@ -21,10 +21,19 @@ class Game
     public static readonly bool DebugCollision = false;
 
     Vector2 Origin = new Vector2(600, 140);
+    bool[,] Map = new bool[20, 20];
 
     public Game()
     {
         Load();
+
+        for (int gy = 0; gy < Map.GetLength(1); gy++)
+        {
+            for (int gx = 0; gx < Map.GetLength(0); gx++)
+            {
+                Map[gx, gy] = true;
+            }
+        }
     }
 
     public void Load()
@@ -44,14 +53,23 @@ class Game
 
         Assets.DefaultFont.Draw(string.Format("Hovered: {0}, {1}", hx, hy), new Vector2(20, 40), Color.White);
 
+        if (Engine.GetMouseButtonDown(MouseButton.Left) &&
+            hx >= 0 && hy < Map.GetLength(0) && hy >= 0 && hy < Map.GetLength(1))
+        {
+            Map[hx, hy] = !Map[hx, hy];
+        }
+
         for (int gy = 0; gy < 20; gy++)
         {
             for (int gx = 0; gx < 20; gx++)
             {
-                Engine.DrawTexture(
-                    Assets.Tiles,
-                    Origin + new Vector2((gx - gy) * 16, (gy + gx) * 8),
-                    source: new Bounds2(0, 0, 32, 32));
+                if (Map[gx, gy])
+                {
+                    Engine.DrawTexture(
+                        Assets.Tiles,
+                        Origin + new Vector2((gx - gy) * 16, (gy + gx) * 8),
+                        source: new Bounds2(0, 0, 32, 32));
+                }
             }
         }
     }
